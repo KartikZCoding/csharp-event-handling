20e49f3a2be989ac7399b4173feba6809adad4fa
KartikZCoding <ahir.kartik.385@gmail.com>
2026-01-12 18:11:41 +0530

    Add project files.

---
+++ EventHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace EventHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Vlog-1" };
+            var videoEncoder = new VideoEncoder();
+            var mailService = new MailService();
+            var messageService = new MessageService();
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+            videoEncoder.Encode(video);
+        }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs e) // VideoEventArgs e)
+        {
+            Console.WriteLine($"MailService: Sending an email...");
+            //Console.WriteLine($"MailService: Sending an email to {e.Video.Title}...");
+        }
+    }
+
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, EventArgs e) // VideoEventArgs e)
+        {
+            Console.WriteLine($"MessageService: Sending a message...");
+            //Console.WriteLine($"MessageService: Sending a message to {e.Video.Title}...");
+        }
+    }
+
+}
---
+++ EventHandling/VideoEncoder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventHandling
+{
+    //public class VideoEventArgs : EventArgs
+    //{
+    //    public Video Video { get; set; }
+    //}
+    public class VideoEncoder 
+    {
+        // 1. Define a delegate
+        //public delegate void VideoEncoderEventHandler(object source, VideoEventArgs args);
+
+        // 2. Define an event based on that delegate
+        public event EventHandler VideoEncoded;
+        //public event EventHandler<VideoEventArgs> VideoEncoded;
+
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine($"Video {video.Title} encoding...!");
+            Thread.Sleep(3000);
+
+            //OnVideoEncoded(video);
+            OnVideoEncoded();
+        }
+
+        // 3. Raise the event
+        protected virtual void OnVideoEncoded() // OnVideoEncoded(Video video)
+        {
+            if(VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+                //VideoEncoded(this, new VideoEventArgs() { Video = video });
+            }
+        }
+    }
+}
